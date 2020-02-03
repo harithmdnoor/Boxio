e1020058dc7a27ef1ec88894c3ddfaaa7d5bedff
Xu Feng Marcus Goh <s10187800@connect.np.edu.sg>
2020-02-03 11:10:10 +0800

    session stats 1 and 2 not working

---
+++ boxio/js/session-stats2.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDEQ5uWfzItrrAf9z6KwZM3YDv5UUgOt0s",
+    authDomain: "portfolio-2-boxio.firebaseapp.com",
+    databaseURL: "https://portfolio-2-boxio.firebaseio.com",
+    projectId: "portfolio-2-boxio",
+    storageBucket: "portfolio-2-boxio.appspot.com",
+    messagingSenderId: "186874167723",
+    appId: "1:186874167723:web:8af804c5e634f83b8692ee",
+    measurementId: "G-HPM59MD6Q7"
+};
+firebase.initializeApp(config);
